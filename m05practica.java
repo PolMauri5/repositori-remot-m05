b381250c87f397e0707e8b855025cf3ba722fc43
pol <polmauri5@gmail.com>
2023-01-26 10:45:34 +0100

    feat: :zap: cnavi pol 3

--- m05practica.java
+++ m05practica.java
@@ -5,6 +5,7 @@ public class m05practica {
         System.out.println("Hola");
         System.out.println("Canvi pol");
         System.out.println("Canvi pol 2");
-        
+        System.out.println("Canvi pol 3");
+
     }
 }

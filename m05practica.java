5051cce9469758c528a9472cc70bcb50b56fbec4
Maxwellnt <pau.martinez@gracia.lasalle.cat>
2023-01-26 10:45:50 +0100

    Methode MenuBoolVerif

--- m05practica.java
+++ m05practica.java
@@ -4,4 +4,41 @@ public class m05practica {
     public static void main(String[] args) {
         System.out.println("Hola");
     }
+
+
+
+    /**
+     * Crea un Menu a través d'un String "picat a pedra" i retorna un boolean  <br>
+     * Utilitza String equals per saber la resposta
+     * @return Retora un true o un false depenent del input
+     */
+    private static boolean MenuBoolVerif (){
+        Scanner llegir = new Scanner(System.in);
+        boolean esAfirmatiu = true;
+        boolean valorsIntroduitsLegals;
+
+        System.out.println("""
+                 Vols comprar un altre billet?\s
+                     ╔══════╗   ╔══════╗  \s
+                     ║  Si  ║   ║  No  ║  \s
+                     ╚══════╝   ╚══════╝  \s
+                Seleciona una opcio: \s""");
+
+        String resposta = llegir.next();
+        do {
+            valorsIntroduitsLegals = true;
+            llegir.nextLine();
+            if (resposta.equalsIgnoreCase("SI")){
+                esAfirmatiu = true;
+
+            } else if (resposta.equalsIgnoreCase("NO")) {
+                esAfirmatiu = false;
+
+            } else {
+                valorsIntroduitsLegals = VarInvalid(); // Donara misatge de error i convertira el boolean en false
+            }
+        }while (!valorsIntroduitsLegals);
+        return esAfirmatiu;
+    }
 }
+

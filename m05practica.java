204a880e5a59420c371f5665b1de0173ffac0297
pol <polmauri5@gmail.com>
2023-01-26 10:43:29 +0100

    feat: :zap: print Canvi pol

--- m05practica.java
+++ m05practica.java
@@ -3,5 +3,6 @@ package com.polmauri.method;
 public class m05practica {
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Canvi pol");
     }
 }

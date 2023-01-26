081fc34575d023808748c30e7356a2f4db297454
Mateo Jubells <mateojubells@gmail.com>
2023-01-26 10:47:35 +0100

    Salutacio

--- m05practica.java
+++ m05practica.java
@@ -3,5 +3,7 @@ package com.polmauri.method;
 public class m05practica {
     public static void main(String[] args) {
         System.out.println("Hola");
+
+        System.out.println("Buenos dias me llamo mateo");
     }
 }

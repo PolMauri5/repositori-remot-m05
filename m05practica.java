9af3de7a911fd60770c6bace1dd9bcb92767032c
Mateo Jubells <mateojubells@gmail.com>
2023-01-26 10:48:34 +0100

    Demanar nom

--- m05practica.java
+++ m05practica.java
@@ -2,8 +2,11 @@ package com.polmauri.method;
 
 public class m05practica {
     public static void main(String[] args) {
+        String nom;
         System.out.println("Hola");
 
         System.out.println("Buenos dias me llamo mateo");
+        System.out.println("diguim el seu nom");
+        // llegir nom
     }
 }

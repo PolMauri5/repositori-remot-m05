aadabf412ee5a2ad84fe26ae3e65ef1de84620a4
Mateo Jubells <mateojubells@gmail.com>
2023-01-26 11:19:00 +0100

    Resolucion problemas

--- m05practica.java
+++ m05practica.java
@@ -1,13 +1,19 @@
 package com.polmauri.method;
 
 public class m05practica {
+
+
     public static void main(String[] args) {
+
+    }
+    public static void mateo(){
+
         String nom;
         System.out.println("Hola");
-
         System.out.println("Buenos dias me llamo mateo");
         System.out.println("diguim el seu nom");
         // llegir nom
         System.out.println("nom");
     }
 }
+
